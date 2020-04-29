7deab05ffaae8b9bd2c1924eb9dd33bb28025554
magdy-dev <markobosh5@gmail.com>
2020-04-29 23:22:31 +0300

    text2 question2

---
+++ epam-tasks/src/main/java/task2/Question10.java
@@ -0,0 +1,11 @@
+package task2;
+
+public class Question10 {
+    public static <n> void main(String[] args) {
+
+
+
+            }
+
+        }
+
---
+++ epam-tasks/src/main/java/task2/Question7.java
@@ -0,0 +1,13 @@
+package task2;
+
+import java.util.Scanner;
+
+public class Question7 {
+    public static <n> void main(String[] args) {
+
+
+
+            }
+
+        }
+
